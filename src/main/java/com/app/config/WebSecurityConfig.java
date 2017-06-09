7204d2d47e8fb39b331902efe0f29fcbad907051
Pasindu De Silva <ppasindud@gmail.com>
2017-06-09 21:05:16 +0800

    remove old code

--- src/main/java/com/app/config/WebSecurityConfig.java
+++ src/main/java/com/app/config/WebSecurityConfig.java
@@ -1,43 +1,3 @@
-// /*
-//  * To change this license header, choose License Headers in Project Properties.
-//  * To change this template file, choose Tools | Templates
-//  * and open the template in the editor.
-//  */
-// package com.app;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.web.savedrequest.NullRequestCache;
-
-// /**
-//  *
-//  * @author Pasindu
-//  */
-
-// @EnableWebSecurity
-// class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-// 	@Override
-// 	protected void configure(HttpSecurity http) throws Exception {
-// 		http.authorizeRequests() //
-// 				.anyRequest().authenticated() //
-// 				.and().requestCache().requestCache(new NullRequestCache()) //
-// 				.and().httpBasic() //
-// 				.and().csrf().disable();
-// 	}
-
-// 	@Autowired
-// 	void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-// 		auth.inMemoryAuthentication() //
-// 				.withUser("user").password("password").authorities("ROLE_USER") //
-// 				.and() //
-// 				.withUser("admin").password("password").authorities("ROLE_USER", "ROLE_ADMIN");
-// 	}
-// }
-
 package com.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -80,12 +40,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
   }
-
-  // @Autowired
-  // void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-  // 	auth.inMemoryAuthentication() //
-  // 			.withUser("user").password("password").authorities("ROLE_USER") //
-  // 			.and() //
-  // 			.withUser("admin").password("password").authorities("ROLE_USER", "ROLE_ADMIN");
-  // }
 }
