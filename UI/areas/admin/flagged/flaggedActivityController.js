f4b16fe47602e3b7977f119026ef6e11ff52f129
Dushan Galappaththi <dcdushan@github.com>
2017-06-10 01:41:24 +0530

    Flagged activity details page added

--- UI/areas/admin/flagged/flaggedActivityController.js
+++ UI/areas/admin/flagged/flaggedActivityController.js
@@ -1,5 +1,14 @@
 'use strict';
 angular.module('banking')
-.controller('FlaggedActivityController',function ($state,$rootScope,$scope) {
-
+.controller('FlaggedActivityController',function ($state,$rootScope,$scope,$stateParams,$http) {
+  var flagid = $stateParams.flagID;
+  if ($stateParams.flagID === ""){
+      $state.transitionTo("login");
+  }
+  else {
+    $http.get('SampleJSON/Flagged/'+ flagid +'.json')
+    .then(function(res) {
+      $scope.flag = res.data[0];
+    });
+  }
 });
