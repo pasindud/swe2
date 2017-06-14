3396fa44246f59f2413073f9c7c7b9cc21b25472
Pasindu De Silva <pasindud@users.noreply.github.com>
2017-06-15 01:54:06 +0800

    Minor update

--- UI/areas/common/datePickerController.js
+++ UI/areas/common/datePickerController.js
@@ -1,17 +1,16 @@
 'use strict';
 angular.module('banking')
-.controller('DatePickerController',function ($state,$rootScope,$scope) {
+.controller('DatePickerController',function ($scope) {
   var currentTime = new Date();
   $scope.currentTime = currentTime;
   $scope.month = $scope.labels.MonthLong,
   $scope.monthShort = $scope.labels.MonthShort;
   $scope.weekdaysFull = $scope.labels.WeekDaysFull;
   $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-  //$scope.disable = [false, 0, 8];
   $scope.today = $scope.labels.Today;
   $scope.clear = $scope.labels.Clear;
   $scope.close = $scope.labels.Close;
-  var days = 365*100;
+  var days = 365 * 100;
   $scope.minDate = (new Date($scope.currentTime.getTime() - ( 1000 * 60 * 60 *24 * days ))).toISOString();
   $scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
   $scope.maxDateToday = (new Date($scope.currentTime.getTime())).toISOString();
