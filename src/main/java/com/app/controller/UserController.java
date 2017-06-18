3800e7d8ad472110131ac10e6aeea0cac66492a5
Pasindu De Silva <ppasindud@gmail.com>
2017-06-18 22:49:31 +0530

    Moved user apis to user controller

---
+++ src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.controller;
+
+import com.app.enties.SpringUserStatic;
+import com.app.repository.UserRepository;
+import com.app.service.UserService;
+import java.util.Collections;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/** @author Pasindu */
+public class UserController {
+  @Autowired private UserService userService;
+
+  @Autowired private UserRepository userRepository;
+
+  @RequestMapping("/api/auth")
+  @GetMapping
+  Map<String, Object> getToken(HttpSession session) {
+    return Collections.singletonMap("session", session.getId());
+  }
+
+  @RequestMapping(value = "/api/registration", method = RequestMethod.POST)
+  public String registration(@RequestBody SpringUserStatic user) {
+    userService.save(user);
+    return "{'status':'ok'}";
+  }
+}
