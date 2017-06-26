4065cc06c2725f5f8ff0b0df43f43949ce5db2a6
Pasindu De Silva <ppasindud@gmail.com>
2017-06-26 08:31:10 +0800

    Added cors support

--- src/main/java/com/app/config/WebSecurityConfig.java
+++ src/main/java/com/app/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.app.config;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,22 +11,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired private UserDetailsService userDetailsService;
+
+  final CorsConfiguration configuration = new CorsConfiguration();
 
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    configuration.setAllowedOrigins(ImmutableList.of("*"));
+    configuration.setAllowedMethods(
+        ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(
+        ImmutableList.of("x-auth-token", "Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
     http.authorizeRequests()
-        .antMatchers("/api/registration")
+        .antMatchers("/api/registration", "/resources/", "/resources/**")
         .permitAll()
         .anyRequest()
         .authenticated()
@@ -33,10 +55,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .requestCache()
         .requestCache(new NullRequestCache())
         .and()
-        .httpBasic()
+        .cors()
         .and()
         .csrf()
-        .disable();
+        .disable()
+        .httpBasic();
   }
 
   @Autowired
