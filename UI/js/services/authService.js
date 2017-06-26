4643d79f112bed3b5770be762de5d73f76880631
Pasindu De Silva <ppasindud@gmail.com>
2017-06-26 08:36:04 +0800

    Added function to get auth token

--- UI/js/services/authService.js
+++ UI/js/services/authService.js
@@ -13,6 +13,15 @@ nnyApp.factory('AuthService',['$http','nnyConst',function ($http,nnyConst) {
     return result;
   }
 
+  var JAVA_ENDPOINT="http://localhost:8080";
+
+  function getAuthToken(username, password) {
+    var headers = {"Authorization": "Basic " + btoa( username + ":" + password)};
+
+    $http.get(JAVA_ENDPOINT + "/api/accounts?id=1", {headers: headers}).then(function (response) {
+      console.log(response);
+    });
+  }
 
 
   function getIsLoggedIn() {
