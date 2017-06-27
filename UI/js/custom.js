a57294bc1d15c2022a0466f51dd976893a6d4305
Dushan Galappaththi <dcdushan@gmail.com>
2017-06-27 12:04:17 +0530

    Error modal and Loading modal
    added

--- UI/js/custom.js
+++ UI/js/custom.js
@@ -2,6 +2,23 @@ $(document).ready(function() {
   //  $('#nv_list').removeAttr('data-toggle');
   $(".button-collapse").sideNav();
 $(".dropdown-button").dropdown();
-
+$('#LoadingModal').modal({
+      dismissible: false,
+      opacity: .7,
+      inDuration: 300,
+      outDuration: 200,
+      startingTop: '0%',
+      endingTop: '40%',
+    }
+  );
+$('#ErrorModal').modal({
+        dismissible: true,
+        opacity: .7,
+        inDuration: 300,
+        outDuration: 200,
+        startingTop: '0%',
+        endingTop: '40%',
+      }
+  );
 
 });
