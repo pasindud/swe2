e25ccf9a3e351b5144fe8f15807b2568b75b1cc1
Pasindu De Silva <ppasindud@gmail.com>
2017-06-29 02:40:42 +0800

    Add Class to get exchange rates

---
+++ src/main/java/com/app/Exceptions/ServerException.java
@@ -0,0 +1,10 @@
+package com.app.Exceptions;
+
+/**
+ * Created by Pasindu on 6/29/17.
+ */
+public class ServerException extends Exception {
+    public ServerException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/app/Exceptions/UserException.java
@@ -0,0 +1,10 @@
+package com.app.Exceptions;
+
+/**
+ * Created by Pasindu on 6/29/17.
+ */
+public class UserException extends Exception{
+    public UserException(String s) {
+        super(s);
+    }
+}
