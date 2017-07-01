e0a74d2e80540a7442c212c50a6f14ed9178a134
Dushan Galappaththi <dcdushan@github.com>
2017-07-01 22:26:35 +0530

    Pagination and Sorting added

---
+++ UI/js/services/sortingService.js
@@ -0,0 +1,18 @@
+/* Author : Dushan Galappaththi */
+var nnyApp = angular.module('banking');
+nnyApp.factory('SortingService',['$filter','nnyConst','$rootScope',function ($filter,nnyConst,$rootScope) {
+  function sortObj(recordSet, sortField, isDescending){
+    var sortedObj = $filter('orderBy')(recordSet, sortField, isDescending);
+    return sortedObj;
+  }
+
+  return {
+    sortObjBy : function (recordSet, sortField, isDescending = false) {
+      return sortObj(recordSet, sortField, isDescending);
+    },
+    sortNumber : function () {
+      //TODO
+      return 1;
+    }
+  }
+}]);
