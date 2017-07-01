ba139f7ed9472529cce2d85d5a8404df1aaa9fd0
Dushan Galappaththi <dcdushan@github.com>
2017-07-02 03:41:03 +0530

    Minor Update

--- UI/areas/account/addBankAccountController.js
+++ UI/areas/account/addBankAccountController.js
@@ -4,7 +4,10 @@ angular.module('banking')
 .controller('AddBankAccountController',function ($state,$rootScope,$scope,$http, $stateParams, AuthService) {
 
   $scope.submitBtn = function (res) {
+    $('#LoadingModal').modal('open');
     //TODO
+
+    $('#LoadingModal').modal('close');
   }
 
 });
