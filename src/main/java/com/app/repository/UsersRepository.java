b0f0a4a13c9f7f087c07cbb6933ab619b89e3d60
Pasindu De Silva <ppasindud@gmail.com>
2017-07-03 03:24:43 +0800

    Update activate to lock

--- src/main/java/com/app/repository/UsersRepository.java
+++ src/main/java/com/app/repository/UsersRepository.java
@@ -1,5 +1,6 @@
 package com.app.repository;
 
+import com.app.enties.Account;
 import com.app.enties.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,13 +9,21 @@ import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface UsersRepository extends JpaRepository<Users, Long> {
   Users findByUsername(String username);
   Users save(Users users);
   Users findByUserId(int userId);
 
+  @Query(
+          value = "Select * From Users",
+          nativeQuery = true
+  )
+  public List<Users> getAllUsers();
+
   @Modifying(clearAutomatically = true)
-  @Query("UPDATE Users c SET c.activate = ?1 WHERE c.userId = ?2")
-  void updateActivate(boolean activate, int user_id);
+  @Query("UPDATE Users c SET c.locked = ?1 WHERE c.userId = ?2")
+  void updateActivate(boolean locked, int user_id);
 }
