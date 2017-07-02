cde08af14d0a018eeab366e99ac894ac0afbe121
Pasindu De Silva <ppasindud@gmail.com>
2017-07-02 21:20:36 +0800

    Added endpoint in the admin UI

--- UI/areas/admin/manageAccounts/userAccountController.js
+++ UI/areas/admin/manageAccounts/userAccountController.js
@@ -1,13 +1,18 @@
 'use strict';
 angular.module('banking')
-.controller('UserAccountController',function ($state,$rootScope,$scope,$http,$stateParams,$filter) {
+.controller('UserAccountController',function ($state,$rootScope,$scope,$http,$stateParams,$filter, AuthService) {
   var userID = $stateParams.userID;
   Materialize.updateTextFields();
   $scope.mode = "edit";
-  debugger;
+  // debugger;
   if ($stateParams.userID === ""){
       $state.transitionTo("login");
   }
+
+  AuthService.getRequest("/api/all_users", null, function (response) {
+    $scope.all_users = response.data;
+  });
+
   else {
     $http.get('SampleJSON/Accounts/'+ userID +'.json')
     .then(function(res) {
