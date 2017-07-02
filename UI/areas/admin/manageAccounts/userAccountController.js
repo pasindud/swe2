27e1e5af1e8359dd3bb3252cf243ab3a053f003b
Pasindu De Silva <ppasindud@gmail.com>
2017-07-03 06:18:05 +0800

    Binded admin view accounts

--- UI/areas/admin/manageAccounts/userAccountController.js
+++ UI/areas/admin/manageAccounts/userAccountController.js
@@ -1,16 +1,6 @@
 'use strict';
 angular.module('banking')
 .controller('UserAccountController',function ($state,$rootScope,$scope,$http,$stateParams,$filter, AuthService) {
-  var userID = $stateParams.userID;
-  Materialize.updateTextFields();
-  $scope.mode = "edit";
-  // debugger;
-  if ($stateParams.userID === ""){
-      $state.transitionTo("login");
-  }
-
-  AuthService.getRequest("/api/all_users", null, function (response) {
-    $scope.all_users = response.data;
-  });
-
+  var userID = $stateParams.account_list;
+  console.log($stateParams.account_list);
 });
