c912c4243361a49925e729017b2c006369f71fbf
Pasindu De Silva <ppasindud@gmail.com>
2017-07-02 21:21:28 +0800

    Added endpoint to get all the accounts

--- UI/areas/admin/manageAccounts/viewAccountsController.js
+++ UI/areas/admin/manageAccounts/viewAccountsController.js
@@ -1,6 +1,11 @@
 'use strict';
 angular.module('banking')
-.controller('ViewAccountsController',function ($state,$rootScope,$scope,$http) {
+.controller('ViewAccountsController',function ($state,$rootScope,$scope,$http, AuthService) {
+
+  AuthService.getRequest("/api/all_acounts", null, function (response) {
+    $scope.all_acounts = response.data;
+  });
+
   $http.get('SampleJSON/Accounts/UsersList.json')
   .then(function(res) {
     $scope.AdminID = res.data[0].AdminID;
