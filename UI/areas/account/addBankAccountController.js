8379db07bd234bf49e2342ee3056b5f3ff66271b
Dushan Galappaththi <dcdushan@github.com>
2017-07-02 16:24:52 +0530

    Update
    - added validation methods for Add Bank Account.
    
    - added validation methods for Add Merchant.

--- UI/areas/account/addBankAccountController.js
+++ UI/areas/account/addBankAccountController.js
@@ -1,12 +1,75 @@
 /* Author : Dushan Galappaththi */
 'use strict';
 angular.module('banking')
-.controller('AddBankAccountController',function ($state,$rootScope,$scope,$http, $stateParams, AuthService) {
+.controller('AddBankAccountController',function ($state,$rootScope,$scope,$http, $stateParams, AuthService, ValidateService) {
+  var isInvalidForm = false;
+  var errors = [""];
+  debugger;
+  function inputFieldAnimate(id,status) {
+    if(status){
+      $("#"+id).addClass('valid');
+      $("#"+id).removeClass('invalid');
+    }else {
+      $("#"+id).addClass('invalid');
+      $("#"+id).removeClass('valid');
+    }
+  }
 
+  function formValidate(field) {
+    if(!field.status)
+    {
+      isInvalidForm = true;
+      errors.push(field.errorMsg);
+    }
+  }
   $scope.submitBtn = function (res) {
     $('#LoadingModal').modal('open');
-    //TODO
+    if($scope.AddBankAccountForm !== undefined){
+      var FormData =$scope.AddBankAccountForm;
+      errors = [""];
+      isInvalidForm = false;
 
+      var accountNumberVal = ValidateService.ValidateAccountNumber(FormData.account_number);
+      var branchVal = ValidateService.ValidateBranch(FormData.branch);
+      var accountBarrierVal = ValidateService.ValidateAccBarrier(FormData.account_barrier);
+      var accountTypeVal = ValidateService.ValidateAccountType(FormData.account_type);
+      var openDateVal = ValidateService.ValidateDate(FormData.account_open_date);
+
+      formValidate(accountNumberVal);
+      formValidate(branchVal);
+      formValidate(accountBarrierVal);
+      formValidate(accountTypeVal);
+      formValidate(openDateVal);
+
+      inputFieldAnimate("account_number",accountNumberVal.status);
+      inputFieldAnimate("branch",branchVal.status);
+      inputFieldAnimate("account_barrier",accountBarrierVal.status);
+      inputFieldAnimate("account_type",accountTypeVal.status);
+      inputFieldAnimate("account_open_date",openDateVal.status);
+
+      if(isInvalidForm)
+      {
+        var errorContent = {
+          Title : "Validation Error",
+          Body : errors
+        }
+        $('#ErrorModal').modal('open');
+        $rootScope.ErrorDialog = errorContent;
+      }
+      else
+      {
+          //TODO Post or Get request
+      }
+    }
+    else {
+      $('#ErrorModal').modal('open');
+      errors.push("Missing Values"," ");
+      var errorContent = {
+        Title : "Validation Error",
+        Body : errors
+      }
+      $rootScope.ErrorDialog= errorContent;
+    }
     $('#LoadingModal').modal('close');
   }
 
--- UI/areas/account/addMerchantAccountController.js
+++ UI/areas/account/addMerchantAccountController.js
@@ -1,7 +1,7 @@
 /* Author : Dushan Galappaththi */
 'use strict';
 angular.module('banking')
-.controller('AddMerchantAccountController',function ($state,$rootScope,$scope,$http, $stateParams, AuthService, validateService) {
+.controller('AddMerchantAccountController',function ($state,$rootScope,$scope,$http, $stateParams, AuthService, ValidateService) {
   var isInvalidForm = false;
   var errors = [""];
   function inputFieldAnimate(id,status) {
@@ -24,23 +24,52 @@ angular.module('banking')
 
   $scope.submitBtn = function (res) {
     $('#LoadingModal').modal('open');
-
+    debugger;
     if($scope.AddMerchantAccountForm !== undefined){
       var FormData =$scope.AddMerchantAccountForm;
       errors = [""];
       isInvalidForm = false;
 
-      //var orgNameVal = ValidateService.
+      var orgNameVal = ValidateService.ValidateOrgName(FormData.org_name);
+      var regNoVal=  ValidateService.ValidateRegNo(FormData.reg_no);
+      var taxNoVal=  ValidateService.ValidateTaxNo(FormData.tax_no);
+      var serviceNameVal =  ValidateService.ValidateServiceName(FormData.service_name);
+      var descVal =  ValidateService.ValidateDesc(FormData.desc);
 
+      formValidate(orgNameVal);
+      formValidate(regNoVal);
+      formValidate(taxNoVal);
+      formValidate(serviceNameVal);
+      formValidate(descVal);
 
-    }else{
-          $('#ErrorModal').modal('open');
-          errors.push("Missing Values"," ");
-          var errorContent = {
-            Title : "Validation Error",
-            Body : errors
-          }
-          $rootScope.ErrorDialog= errorContent;
+      inputFieldAnimate("org_name",orgNameVal.status);
+      inputFieldAnimate("reg_no",regNoVal.status);
+      inputFieldAnimate("tax_no",taxNoVal.status);
+      inputFieldAnimate("service_name",serviceNameVal.status);
+      inputFieldAnimate("desc",descVal.status);
+      if(isInvalidForm)
+      {
+        var errorContent = {
+          Title : "Validation Error",
+          Body : errors
+        }
+        $('#ErrorModal').modal('open');
+        $rootScope.ErrorDialog = errorContent;
+      }
+      else
+      {
+        //TODO Post or Get request
+      }
+    }
+    else
+    {
+      $('#ErrorModal').modal('open');
+      errors.push("Missing Values"," ");
+      var errorContent = {
+        Title : "Validation Error",
+        Body : errors
+      }
+      $rootScope.ErrorDialog= errorContent;
     }
     $('#LoadingModal').modal('close');
   }
