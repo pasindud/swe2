37babb4017f76ec02bab9aa9772d039d4f29ba96
Pasindu De Silva <ppasindud@gmail.com>
2017-07-06 07:41:41 +0800

    Added recover client logic

--- UI/areas/login/recoverAccountController.js
+++ UI/areas/login/recoverAccountController.js
@@ -1,7 +1,28 @@
 /* Author : Dushan Galappaththi */
 'use strict';
 angular.module('banking')
-  .controller('RecoverAccountController', function($state, $rootScope, $scope, $http, $stateParams, AuthService) {
+.controller('RecoverAccountController', function($state, $rootScope, $scope, $http, $stateParams, AuthService) {
 
+	AuthService.getRequest("/api/ui_all_questions", null, function(response) {
+		console.log(response.data);
+		$scope.questionslist = response.data; 
+	});
 
-  });
+	$scope.recoverbtn = function () {
+
+		var data = {
+			username : $scope.username,
+			answers : [
+			{
+				"id":$scope.FormData.question1,
+				"answer":$scope.FormData.answer1
+			},
+			{
+				"id":$scope.FormData.question2,
+				"answer": $scope.FormData.answer2
+			}
+			]};
+
+		}
+
+	});
