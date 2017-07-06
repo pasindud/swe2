3dbea2b6ea9d58d7e72a1d7688d5ef089bd921aa
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-07 02:27:46 +0530

    Account activity table update

--- UI/areas/activity/activityController.js
+++ UI/areas/activity/activityController.js
@@ -1,8 +1,7 @@
 /* Author : Dushan Galappaththi */
 'use strict';
 angular.module('banking')
-  .controller('ActivityController', function($state, $rootScope, $scope, $http, $stateParams, AuthService, PaginationService, SortingService) {
-
+  .controller('ActivityController', function($state, $rootScope, $scope, $http, $stateParams, AuthService, PaginationService, SortingService, SearchService) {
 
     var transactionId = $stateParams.transactionId;
     if (!transactionId) {
@@ -10,6 +9,10 @@ angular.module('banking')
       return;
     };
     AuthService.getRequest("/api/transactions?id=" + transactionId, null, function(response) {
+      var responseData = response.data;
+      console.log(responseData);
+
+      //Handle error from server
       if (response.data.error) {
         $('#ErrorModal').modal('open');
         var errorContent = {
@@ -19,36 +22,49 @@ angular.module('banking')
         $rootScope.ErrorDialog = errorContent;
       } else {
         $scope.Transactions = response.data;
-        handlePagination($scope.Transactions); 
+        handlePagination($scope.Transactions);
       }
+
+      //Keep Original Order for sorting purpose
+      $scope.TransactionsOriginal = responseData; //View
+
     });
 
     var paginationObj = "";
     var pageNumbers = "";
+    var maxRecordsPerPage = 10;
     $scope.CurrentPage = 1;
 
+    //Pagination
     function handlePagination(recordSet) {
-      paginationObj = PaginationService.pagination(recordSet, 10);
-      $scope.Transactions = paginationObj.pages[1];
+      paginationObj = PaginationService.pagination(recordSet, maxRecordsPerPage);
+      $scope.Transactions = paginationObj.pages[1]; //View
       pageNumbers = PaginationService.pageNumbers(paginationObj.pageCount);
       $scope.PageNumbers = pageNumbers;
     }
-    //sample data
-    //TODO replace this
-    // $http.get('SampleJSON/Activity/Activity1.json')
-    // .then(function(res) {
-    // 	$scope.UserID = res.data[0].UserID;
-    // 	$scope.TransactionsOriginal = res.data[0].Transactions;
-    // 	handlePagination($scope.TransactionsOriginal);
-    // });
 
     //Sorting Button
+    var fromField ="";
+    var isDsc= false;
     $scope.sortBtn = function(field, ascending) {
-      $('#LoadingModal').modal('open');
-      var recordSet = $scope.TransactionsOriginal;
-      var sortedRecordSet = SortingService.sortObjBy(recordSet, field);
+      debugger;
+      //Determine whether its descending or ascending
+      if(fromField === field)
+      {
+        if(isDsc){
+          isDsc = false;
+        }else {
+          isDsc = true;
+        }
+      }else {
+        fromField = field;
+        isDsc = false;
+      }
+
+      var recordSet = $scope.TransactionsOriginal; //View Ori
+      var sortedRecordSet = SortingService.sortObjBy(recordSet, field, isDsc);
+      $scope.sortedRecordSet = sortedRecordSet;
       handlePagination(sortedRecordSet);
-      $('#LoadingModal').modal('close');
     }
 
     //Pagination button
@@ -56,14 +72,20 @@ angular.module('banking')
       if (typeof toPage == "undefined" || typeof fromPage == "undefined") {
         return;
       }
+      //reset Pages
+      for(i=1;i<pageNumbers.length;i++)
+      { //skip 1
+        pageNumbers[i].isCurrent = false;
+      }
 
-      $('#LoadingModal').modal('open');
-      pageNumbers[fromPage].isCurrent = false;
       pageNumbers[toPage].isCurrent = true;
       $scope.CurrentPage = toPage;
       $scope.PageNumbers = pageNumbers;
-      $scope.Transactions = paginationObj.pages[toPage];
-      $('#LoadingModal').modal('close');
+      $scope.Transactions = paginationObj.pages[toPage]; //View
+      $('#LoadingModal').modal('open');
+      var a = setTimeout(function () {
+        $('#LoadingModal').modal('close');
+      },1000);
     }
 
     //View more
@@ -72,6 +94,29 @@ angular.module('banking')
         transactionId: transactionId
       });
     }
+
+    //Search
+    //TODO add proper search fields
+     var searchFields = [];
+     searchFields.push({id : 'username', name : 'Username'});
+     searchFields.push({id : 'userId', name : 'User ID'});
+     $scope.SearchFields = searchFields;
+
+     //default
+     var searchDataObj = {SearchField : 'username', SearchValue : ''}
+     $scope.SearchData = searchDataObj;
+
+     $scope.$watchCollection('SearchData', function() {
+       if($scope.SearchData!== undefined){
+         if(($scope.SearchData.SearchValue!=="" )&&($scope.SearchData.SearchValue!==undefined)){
+           var searchField = $scope.SearchData.SearchField;
+           var searchValue = $scope.SearchData.SearchValue;
+           var searchResult = SearchService.search($scope.TransactionsOriginal,searchField,searchValue);
+           handlePagination(searchResult);
+         }
+       }
+
+     });
 
 
   });
