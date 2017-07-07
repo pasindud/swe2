e5dca5c77d77a9e881909dce34c42ef6bef4713b
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-07 16:30:45 +0530

    ui minimum date update

--- UI/areas/common/datePickerController.js
+++ UI/areas/common/datePickerController.js
@@ -11,14 +11,15 @@ angular.module('banking')
 
     $scope.currentTime = currentTime;
     $scope.month = $scope.labels.MonthLong,
-      $scope.monthShort = $scope.labels.MonthShort;
+    $scope.monthShort = $scope.labels.MonthShort;
     $scope.weekdaysFull = $scope.labels.WeekDaysFull;
     $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     $scope.today = $scope.labels.Today;
     $scope.clear = $scope.labels.Clear;
     $scope.close = $scope.labels.Close;
     var days = 365 * 100;
-    $scope.minDate = (new Date($scope.currentTime.getTime() - (1000 * 60 * 60 * 24 * days))).toISOString();
+    //$scope.minDate = (new Date($scope.currentTime.getTime() - (1000 * 60 * 60 * 24 * days))).toISOString();
+    $scope.minDate = (new Date("01/01/1700")).toISOString();
     $scope.maxDate = (new Date($scope.currentTime.getTime() + (1000 * 60 * 60 * 24 * days))).toISOString();
     $scope.maxDateToday = (new Date($scope.currentTime.getTime())).toISOString();
   });
