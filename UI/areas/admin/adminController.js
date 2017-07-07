b88ac82cb52adecfe269d1e38a9dfdf74c94a0c2
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-08 01:01:44 +0530

    admin update

--- UI/areas/admin/adminController.js
+++ UI/areas/admin/adminController.js
@@ -1,5 +1,32 @@
+/* Author : Dushan Galappaththi */
 'use strict';
 angular.module('banking')
-  .controller('AdminController', function($state, $rootScope, $scope) {
+  .controller('AdminController', function($state, $rootScope, $scope, AuthService) {
+    //request to server
+    AuthService.getRequest("/api/admin/get_suspicious_logs", null, function(response) {
+      var responseData = response.data;
+      $scope.Logs = "NOACTIVTY";
+      //Handle error from server
+      if (response.data.error) {
+        $scope.Logs = "NOACTIVTY";
+        $scope.noSuspicious = true;
+      } else {
+        if(response.data == null)
+        {
+          $scope.Logs = "NOACTIVTY";
+          $scope.noSuspicious = true;
+        }else {
+          var len = responseData.length;
+          if (len>3) {
+            $scope.Logs = responseData.slice(-3);
+          }else {
+              $scope.Logs = responseData;
+          }
+          $scope.noSuspicious = false;
+        }
+      }
+      //Keep Original Order for sorting purpose
+      $scope.LogsOriginal = responseData; //View
+    });
 
-  });
+});
