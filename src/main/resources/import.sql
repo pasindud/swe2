db355b4d79d2b7d1ac8d917625396ee6994448c6
Pasindu De Silva <ppasindud@gmail.com>
2017-07-08 05:43:35 +0800

    more

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -22,3 +22,40 @@ INSERT INTO `account_type` (`acc_type_id`, `acc_interest_rates`, `acc_name`, `da
 (1, 3.2, 'Savings', 50000, NULL, NULL, NULL, 500, NULL),
 (2, 2, 'Current Account', NULL, NULL, 50000, 100000, 1000000, NULL),
 (3, 3, 'Fixed Disposit', NULL, 24, NULL, 3000, 50000, 12);
+
+
+-- 
+
+
+INSERT INTO `customer` (`customerid`, `address_line1`, `address_line2`, `address_line3`, `city`, `dob`, `email`, `fax_no`, `first_name`, `gender`, `last_name`, `mobile_no`, `nic`, `telephone_no`, `title`)
+VALUES
+    (1, 'rUKVrIm0hVXBAmn6MoU7zg==', NULL, NULL, 'wb5ELIgAjUET9BLTxLxjIQ==', NULL, 'kJScSvtzM0tB36PSse8GdAqlammTmdqyt4YF9wbvv/s=', NULL, 'OrL5aKAP2Kan5BQoXyGoHA==', NULL, '2fCrdZwn3DPDrPRVXOdauw==', NULL, '5ugCjoAV1CZlF6PshvnrBA==', NULL, NULL);
+
+-- testinguser1 testpassword
+INSERT INTO `users` (`user_id`, `creation_date`, `locked`, `password`, `user_type`, `username`, `customer_id`, `merchant_merchantid`)
+VALUES
+    (1, '2017-07-08 05:29:32', 1, '$2a$10$7p.ELkhd6nROL1L12MVsRu8kSEj2mJTxe8aCNxryNamimqKHMR932', 'CUSTOMER', 'testinguser1', 1, NULL);
+
+INSERT INTO `users` (`user_id`, `creation_date`, `locked`, `password`, `user_type`, `username`, `customer_id`, `merchant_merchantid`)
+VALUES
+    (2, '2017-07-08 05:29:32', 1, '$2a$10$7p.ELkhd6nROL1L12MVsRu8kSEj2mJTxe8aCNxryNamimqKHMR932', 'CUSTOMER', 'ADMIN', NULL, NULL);
+
+
+INSERT INTO `user_role` (`users_user_id`, `role_id`)
+VALUES
+    (1, 1),
+    (2, 2);
+
+INSERT INTO `account` (`accountid`, `balance`, `creation_date`, `currency`, `expire_date`, `locked`, `acctypeid`, `branchid`, `userid`)
+VALUES
+    (1, 10000, '2017-07-08 05:39:12', 'EUR', NULL, 0, 1, 1, 1);
+
+INSERT INTO `account` (`accountid`, `balance`, `creation_date`, `currency`, `expire_date`, `locked`, `acctypeid`, `branchid`, `userid`)
+VALUES
+    (2, 10000, '2017-07-08 05:39:12', 'USD', NULL, 0, 1, 1, 1);
+
+
+INSERT INTO merchant (merchantid, logourl, orgname, registrationno, taxno) VALUES (1,'non','Dialog','1232','12313');
+INSERT INTO merchant_services (serviceid, merchantid,accountid,  servicename) VALUES (1,1,2, "New Service");
+
+
