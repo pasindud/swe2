b45625acf6c792fb3d6f7d5fd787c0d3b6f9b206
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-07 21:54:41 +0530

    bank name

--- UI/js/services/constants.js
+++ UI/js/services/constants.js
@@ -2,7 +2,8 @@ var nnyApp = angular.module('banking');
 
 nnyApp.constant('nnyConst', {
     test: "hi",
-    ENDPOINT_URI: "http://localhost:8080",
+    title : " Bank Of Srilanka",
+    ENDPOINT_URI: "http://192.168.1.134:8080",
     BAD_TOKEN_ERROR: "Full authentication is required to access this resource.",
     UserRoles: {
         Admin: '3',
