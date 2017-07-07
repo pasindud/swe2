3023a8912979e0cc1c1a01e35459b6813cf2a414
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-08 00:18:06 +0530

    Suspicious logs binding

--- UI/areas/admin/flagged/flagsController.js
+++ UI/areas/admin/flagged/flagsController.js
@@ -1,9 +1,110 @@
 'use strict';
 angular.module('banking')
-.controller('FlagsController',function ($state,$rootScope,$scope,$http) {
-  $http.get('SampleJSON/Flagged/flaggedList.json')
-  .then(function(res) {
-    $scope.AdminID = res.data[0].AdminID;
-    $scope.FlaggedList = res.data[0].FlaggedList;
-  })
+.controller('FlagsController',function ($state,$rootScope,$scope,$http,AuthService,PaginationService,SortingService,SearchService) {
+
+  //request to server
+  AuthService.getRequest("/api/admin/get_suspicious_logs", null, function(response) {
+    var responseData = response.data;
+
+    //Handle error from server
+    if (response.data.error) {
+      $('#ErrorModal').modal('open');
+      var errorContent = {
+        Title: "Error",
+        Body: [response.data.error]
+      }
+      $rootScope.ErrorDialog = errorContent;
+    } else {
+      $scope.Logs = response.data;
+      handlePagination($scope.Logs); //View
+    }
+
+    //Keep Original Order for sorting purpose
+    $scope.LogsOriginal = responseData; //View
+  });
+
+  var paginationObj = "";
+  var pageNumbers = "";
+  var maxRecordsPerPage = 10;
+  $scope.CurrentPage = 1;
+
+  //Pagination
+  function handlePagination(recordSet) {
+    paginationObj = PaginationService.pagination(recordSet, maxRecordsPerPage);
+    $scope.Logs = paginationObj.pages[1]; //View
+    pageNumbers = PaginationService.pageNumbers(paginationObj.pageCount);
+    $scope.PageNumbers = pageNumbers;
+  }
+
+  //Sorting Button
+  var fromField ="";
+  var isDsc= false;
+  $scope.sortBtn = function(field, ascending) {
+    debugger;
+    //Determine whether its descending or ascending
+    if(fromField === field)
+    {
+      if(isDsc){
+        isDsc = false;
+      }else {
+        isDsc = true;
+      }
+    }else {
+      fromField = field;
+      isDsc = false;
+    }
+
+    var recordSet = $scope.LogsOriginal; //View Ori
+    var sortedRecordSet = SortingService.sortObjBy(recordSet, field, isDsc);
+    $scope.sortedRecordSet = sortedRecordSet;
+    handlePagination(sortedRecordSet);
+  }
+
+  //Pagination button
+  $scope.pageBtn = function(toPage, fromPage) {
+    if (typeof toPage == "undefined" || typeof fromPage == "undefined") {
+      return;
+    }
+    //reset Pages
+    for(i=1;i<pageNumbers.length;i++)
+    { //skip 1
+      pageNumbers[i].isCurrent = false;
+    }
+
+    pageNumbers[toPage].isCurrent = true;
+    $scope.CurrentPage = toPage;
+    $scope.PageNumbers = pageNumbers;
+    $scope.Logs = paginationObj.pages[toPage]; //View
+    $('#LoadingModal').modal('open');
+    var a = setTimeout(function () {
+      $('#LoadingModal').modal('close');
+    },1000);
+  }
+
+  //Search
+   var searchFields = [];
+   searchFields.push({id : 'id', name : 'Violation ID'});
+   searchFields.push({id : 'message', name : 'Message'});
+   searchFields.push({id : 'createdDate', name : 'Created Date'});
+   $scope.SearchFields = searchFields;
+
+   //default
+   var searchDataObj = {SearchField : 'message', SearchValue : ''}
+   $scope.SearchData = searchDataObj;
+
+   $scope.$watchCollection('SearchData', function() {
+     if($scope.SearchData!== undefined){
+       if(($scope.SearchData.SearchValue!=="" )&&($scope.SearchData.SearchValue!==undefined)){
+         var searchField = $scope.SearchData.SearchField;
+         var searchValue = $scope.SearchData.SearchValue;
+         var searchResult = SearchService.search($scope.LogsOriginal,searchField,searchValue); //View
+         handlePagination(searchResult);
+       }
+       else {
+         handlePagination($scope.LogsOriginal); //view
+       }
+     }
+
+   });
+
 });
