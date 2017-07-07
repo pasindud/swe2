37a59fdf146ef7031eba4611197b7ba674225766
Dushan Galappaththi <dcdushan@gmail.com>
2017-07-07 23:15:54 +0530

    Update

--- UI/areas/admin/manageUsers/viewUserProfileController.js
+++ UI/areas/admin/manageUsers/viewUserProfileController.js
@@ -4,17 +4,31 @@ angular.module('banking')
   var userId = $stateParams.userid;
   $scope.PersonalInformation;
   $scope.selectedAccountDetails;
+  requestServerData();
 
-  AuthService.getRequest("/api/admin/user_by_id?id=" + userId, null, function (response) {
-    $scope.PersonalInformation = response.data;
-  });
+  function requestServerData() {
+    AuthService.getRequest("/api/admin/user_by_id?id=" + userId, null, function (response) {
+      $scope.PersonalInformation = response.data.customer;
+      $scope.PersonalInformation.userObj = response.data;
+      var addressList = [$scope.PersonalInformation.addressLine1,$scope.PersonalInformation.addressLine2,$scope.PersonalInformation.addressLine3];
+      $scope.PersonalInformation.wholeAddress = addressList.join(', ');
+    });
+    //request to server
+    AuthService.getRequest("/api/admin/account__by_user_id?userid=" + userId, null, function(response) {
+      $scope.user_accounts = response.data;
+      console.log("Account" + userId);
+      console.log(response.data);
+    });
+  }
 
-  //request to server
-  AuthService.getRequest("/api/accounts?id=" + userId, null, function(response) {
-    $scope.user_accounts = response.data;
-    console.log("Account" + userId);
-    console.log(response.data);
-  });
+  function lockRequest(userId,isLock) {
+    //request to server
+    AuthService.getRequest("/api/admin/change_user_status?user_id=" + userId + "&lock="+isLock, null, function(response) {
+      console.log("lock/unlock tring");
+      console.log(response.data);
+    });
+  }
+
 
   //when user selects their account from drop down
   $scope.setAccountDetails = function() {
@@ -26,6 +40,14 @@ angular.module('banking')
       var addressList = [details.branchId.addressLine1,details.branchId.addressLine2,details.branchId.addressLine3]
       $scope.selectedAccountDetails.branchId.wholeAddress = addressList.join(', ');
     }
+  }
+
+  $scope.btnLock = function () {
+    lockRequest(userId,true);
+  }
+
+  $scope.btnUnlock = function () {
+    lockRequest(userId,false);
   }
 
 
